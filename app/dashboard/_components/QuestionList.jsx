b48b367520c317ad4dc78376cc322c60e48ed245
unknown <shreyaankapoor@gmail.com>
2025-07-02 18:18:16 +0530

    devops changes

--- app/dashboard/_components/QuestionList.jsx
+++ app/dashboard/_components/QuestionList.jsx
@@ -29,7 +29,7 @@ const QuestionList = () => {
     <div>
       {questionList.length > 0 ? (
         <>
-          <h2 className="font-medium text-xl">Previous Mock Interview</h2>
+          <h2 className="font-medium text-xl">Previous Generated Questions</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-3">
             {questionList.map((question, index) => (
               <QuestionItemCard key={index} question={question} />
